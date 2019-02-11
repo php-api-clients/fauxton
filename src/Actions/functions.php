6c9ca10e7170de028fd490cbfb191aae3e74ab39
ace411 <lochbm@live.com>
2019-02-11 07:23:42 +0300

    Added missing namespace

--- src/Actions/functions.php
+++ src/Actions/functions.php
@@ -2,6 +2,8 @@
 
 namespace Chemem\Fauxton\Actions;
 
+use \Chemem\Bingo\Functional\Algorithms as A;
+
 function updateMerge(array $original, array $update) : array
 {
     $update = A\compose(A\partial(A\omit, '_id', '_rev'), A\partial(A\extend, $update));
